5d061d77e5bac4db633e2ef545ef293f15c2c239
Mark Hjorth Sørensen <MarkHjorthSorensen@icloud.com>
2018-02-09 23:37:45 +0100

    Changed apple-mobile-web-app-capable from 'true' to 'yes' - hopefully it will work now. Added startup images for web apps on homescreen

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,12 +17,22 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Mark Hjorth Does Software Development, Game Development, Android App Development, Web Development, Quality Assurance, Creates Distributed Systems, and more...' },
       { name: 'nativeUI', content: 'true' },
       { name: 'HandheldFriendly', content: 'true' },
-      { name: 'apple-mobile-web-app-capable', content: 'true'},
+      { name: 'apple-mobile-web-app-capable', content: 'yes'},
       { name: 'apple-mobile-web-app-status-bar-style', content: 'default'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'canonical', href: 'https://beta.markhjorth.com' }
+      { rel: 'canonical', href: 'https://beta.markhjorth.com' },
+      { rel: 'apple-touch-startup-image', media: '(device-width: 320px) and (device-height: 480px) and (-webkit-device-pixel-ratio: 1)', href: './favicons/apple-touch-startup-image-320x460.png'},
+      { rel: 'apple-touch-startup-image', media: '(device-width: 320px) and (device-height: 480px) and (-webkit-device-pixel-ratio: 2)', href: './favicons/apple-touch-startup-image-640x920.png'},
+      { rel: 'apple-touch-startup-image', media: '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)', href: './favicons/apple-touch-startup-image-640x1096.png'},
+      { rel: 'apple-touch-startup-image', media: '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)', href: './favicons/apple-touch-startup-image-750x1294.png'},
+      { rel: 'apple-touch-startup-image', media: '(device-width: 414px) and (device-height: 736px) and (orientation: landscape) and (-webkit-device-pixel-ratio: 3)', href: './favicons/apple-touch-startup-image-1182x2208.png'},
+      { rel: 'apple-touch-startup-image', media: '(device-width: 414px) and (device-height: 736px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 3)', href: './favicons/apple-touch-startup-image-1242x2148.png'},
+      { rel: 'apple-touch-startup-image', media: '(device-width: 768px) and (device-height: 1024px) and (orientation: landscape) and (-webkit-device-pixel-ratio: 1)', href: './favicons/apple-touch-startup-image-748x1024.png'},
+      { rel: 'apple-touch-startup-image', media: '(device-width: 768px) and (device-height: 1024px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 1)', href: './favicons/apple-touch-startup-image-768x1004.png'},
+      { rel: 'apple-touch-startup-image', media: '(device-width: 768px) and (device-height: 1024px) and (orientation: landscape) and (-webkit-device-pixel-ratio: 2)', href: './favicons/apple-touch-startup-image-1496x2048.png'},
+      { rel: 'apple-touch-startup-image', media: '(device-width: 768px) and (device-height: 1024px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 2)', href: './favicons/apple-touch-startup-image-1536x2008.png'},
     ]
   },
   /*
