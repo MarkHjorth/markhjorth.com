c961dfddce3a7aa23c9568d5ba413c601720fb73
Mark Hjorth Sørensen <MarkHjorthSorensen@icloud.com>
2018-06-09 15:08:27 +0200

    Added 'Android App' category and added post 'Wireless Automation App for Automatic Android Testing'

---
+++ pages/portfolio/android-apps/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="container">
+    <div class="content">
+      <h1 class="title">
+        Android App Development
+      </h1>
+      <p>Android apps are a big part of our new modern life.</p>
+      <p>We use mobile devices more and more, which makes Android apps even more important than ever before.</p>
+      <p>On this page you can find an automation app created to automatically test wireless signals from Android phones, as well as my latests work.</p>
+      <p>Below you can find my latest projects in Android app development:</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Android App Development and my Android apps - Mark Hjorth'
+  }
+}
+</script>
---
+++ pages/portfolio/android-apps/wireless-automation-app.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container">
+    <article>
+
+      <h1 class="title">
+        Wireless Automation App for Automatic Android Testing
+      </h1>
+      <p>This wireless automation app for android was developed for Rohde & Schwarz to automate wireless tests on Android phones.</p>
+      <p>This app explores the options for controlling an Android phone from a computer. The application is be able to control a range of settings and features in the Android device, such as sending texts and toggling the Flightmode option.</p>
+      <p>The technologies in the app includes, but are not limited to, socket programming, ADB, basic Android development and threads.</p>
+      <p>Installing this software allows anyone to set up a controlling computer and pair it with an android phone. The computer will then be able to send testing commands to the phone for automatic wireless tests.</p>
+      <p>A report for the development of this app can be found at UC Viden.</p>
+      <p>The projects source code can be found on my GitHub page here.</p>
+      
+    </article>
+  </section>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Wireless Automation App for Automatic Android Testing - Mark Hjorth'
+  }
+}
+</script>
