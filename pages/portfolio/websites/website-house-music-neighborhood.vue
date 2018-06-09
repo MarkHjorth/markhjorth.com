68afef35f472be17baa932ce07772706a226ac66
Mark Hjorth Sørensen <MarkHjorthSorensen@icloud.com>
2018-06-09 14:58:03 +0200

    Added post for House of Music Neighborhood Website

---
+++ pages/portfolio/websites/website-house-music-neighborhood.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="container">
+    <article>
+
+      <h1 class="title">
+        Website for the House of Music Neighborhood
+      </h1>
+      <p>This website was created for the House of Music neighborhood and the people living there. The sites main focus is to allow the owners association to communicate on a shared platform as well as share important documents and reports from meetings.</p>
+    
+    </article>
+  </section>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'House of Music Neighborhood Website - Mark Hjorth'
+  }
+}
+</script>
