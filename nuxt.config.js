025698f5b0706a0ca511cdd5e765af38c03b3f26
Mark Hjorth <sorensenmh@elsevier.com>
2020-03-29 13:39:00 +0200

    Updated dependencies and meta data

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,34 +4,25 @@ import axios from 'axios'
 export default {
   mode: 'universal',
 
+  env: {
+    websiteUrl: "https://markhjorth.com"
+  },
+
   /*
   ** Headers of the page
   */
   head: {
     title: 'Mark Hjorth - Software, Game and Web Development',
-    meta: [
-    { charset: 'utf-8' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1, minimum-scale=1'},
-    { hid: 'description', name: 'description', content: pkg.description },
-    { name: 'nativeUI', content: 'true' },
-    { name: 'HandheldFriendly', content: 'true' },
-    { name: 'apple-mobile-web-app-capable', content: 'yes'},
-    { name: 'apple-mobile-web-app-status-bar-style', content: 'default'}
 
+    meta: [
+      { name: 'nativeUI', content: 'true' },
+      { name: 'apple-mobile-web-app-capable', content: 'yes'},
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'default'}
     ],
+
     link: [
-    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-    { rel: 'canonical', href: 'https://beta.markhjorth.com' },
-    { rel: 'apple-touch-startup-image', media: '(device-width: 320px) and (device-height: 480px) and (-webkit-device-pixel-ratio: 1)', href: './favicons/apple-touch-startup-image-320x460.png'},
-    { rel: 'apple-touch-startup-image', media: '(device-width: 320px) and (device-height: 480px) and (-webkit-device-pixel-ratio: 2)', href: './favicons/apple-touch-startup-image-640x920.png'},
-    { rel: 'apple-touch-startup-image', media: '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)', href: './favicons/apple-touch-startup-image-640x1096.png'},
-    { rel: 'apple-touch-startup-image', media: '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)', href: './favicons/apple-touch-startup-image-750x1294.png'},
-    { rel: 'apple-touch-startup-image', media: '(device-width: 414px) and (device-height: 736px) and (orientation: landscape) and (-webkit-device-pixel-ratio: 3)', href: './favicons/apple-touch-startup-image-1182x2208.png'},
-    { rel: 'apple-touch-startup-image', media: '(device-width: 414px) and (device-height: 736px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 3)', href: './favicons/apple-touch-startup-image-1242x2148.png'},
-    { rel: 'apple-touch-startup-image', media: '(device-width: 768px) and (device-height: 1024px) and (orientation: landscape) and (-webkit-device-pixel-ratio: 1)', href: './favicons/apple-touch-startup-image-748x1024.png'},
-    { rel: 'apple-touch-startup-image', media: '(device-width: 768px) and (device-height: 1024px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 1)', href: './favicons/apple-touch-startup-image-768x1004.png'},
-    { rel: 'apple-touch-startup-image', media: '(device-width: 768px) and (device-height: 1024px) and (orientation: landscape) and (-webkit-device-pixel-ratio: 2)', href: './favicons/apple-touch-startup-image-1496x2048.png'},
-    { rel: 'apple-touch-startup-image', media: '(device-width: 768px) and (device-height: 1024px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 2)', href: './favicons/apple-touch-startup-image-1536x2008.png'}
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'canonical', href: 'https://beta.markhjorth.com' },
     ]
   },
 
@@ -82,6 +73,17 @@ export default {
   */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+  ** PWA Configuration
+  */
+  pwa: {
+    manifest: {
+      name: 'Mark Hjorth',
+      short_name: "Mark Hjorth",
+      start_url: "/"
+    }
   },
 
   /*
